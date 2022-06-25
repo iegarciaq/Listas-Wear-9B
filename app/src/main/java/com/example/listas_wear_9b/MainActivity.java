dc182b823b100040c20863c4346ce3310a21942e
IvanGQ <ivan.garcia@utxicotepec.edu.mx>
2022-06-25 09:04:16 -0500

    Proyecto terminado

--- app/src/main/java/com/example/listas_wear_9b/MainActivity.java
+++ app/src/main/java/com/example/listas_wear_9b/MainActivity.java
@@ -4,20 +4,51 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import androidx.wear.widget.WearableLinearLayoutManager;
+import androidx.wear.widget.WearableRecyclerView;
+
 import com.example.listas_wear_9b.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
-    private TextView mTextView;
-    private ActivityMainBinding binding;
+    ArrayList<Contacto> contactos = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        WearableRecyclerView recyclerView =
+                findViewById(R.id.recycler_launcher_view);
 
-        mTextView = binding.text;
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setEdgeItemsCenteringEnabled(true);
+        recyclerView.setLayoutManager(
+                new WearableLinearLayoutManager(this));
+
+        contactos.add(
+                new Contacto(R.drawable.avatars_uno, "Ivan")
+        );  contactos.add(
+                new Contacto(R.drawable.avatars_dos, "Eduardo")
+        );
+
+        recyclerView.setAdapter(new
+                ContactosAdapter(contactos, this, new ContactosAdapter.AdapterCallback() {
+            @Override
+            public void onItemClicked(Integer itemPosition) {
+                System.out.println(itemPosition);
+                // Alerta
+
+                switch (itemPosition){
+                    case 0:
+                        break;
+                    case 1:
+                    break;
+                }
+            }
+        }));
+
     }
 }
