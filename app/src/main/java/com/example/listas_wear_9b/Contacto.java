e85c232851657baf8178040dda97a6f43f342062
IvanGQ <ivan.garcia@utxicotepec.edu.mx>
2022-06-25 07:39:30 -0500

    Carga inicial del proyecto

---
+++ app/src/main/java/com/example/listas_wear_9b/Contacto.java
@@ -0,0 +1,20 @@
+package com.example.listas_wear_9b;
+
+public class Contacto {
+
+    private int image;
+    private String name;
+
+    public Contacto(int image, String name) {
+        this.image = image;
+        this.name = name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
